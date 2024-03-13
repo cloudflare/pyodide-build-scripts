fc5a7f2c61cbe7844cc902dcfd12e6940c6f90d2
Garrett Gu <garrett@cloudflare.com>
2024-03-13 21:27:40 +0000

    fix pyodide linkage, print sha sum

--- pyodide/prelude.js
+++ pyodide/prelude.js
@@ -2,6 +2,7 @@ import {
   monotonicDateNow,
   newWasmModule,
   wasmInstantiate,
+  getRandomValues,
 } from "pyodide-internal:builtin_wrappers";
 const document = undefined;
 const location = undefined;
