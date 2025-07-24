392832890b151b975ae03f35a2eddbdb33a3ee75
Hood Chatham <roberthoodchatham@gmail.com>
2025-07-24 17:14:28 +0200

    Use script from pyodide-recipes to install and patch emsdk

--- packages/setup.sh
+++ packages/setup.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/emscripten-core/emsdk
-cd emsdk
-./emsdk install $EMSDK_VERSION
-./emsdk activate $EMSDK_VERSION
-source ./emsdk_env.sh
-cd ..
-
 git clone https://github.com/pyodide/pyodide-recipes.git
 (cd pyodide-recipes && git checkout $PYODIDE_TAG)
+
 ln -s pyodide-recipes/packages packages
 
 # rust is required for building some wheels
@@ -24,3 +18,4 @@ source .venv/bin/activate
 pip install pyodide-build
 
 pip install boto3 cython requests
+python pyodide-recipes/tools/install_and_patch_emscripten.py
